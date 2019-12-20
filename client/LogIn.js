76099417ade574aa4fb42b99748189a5fa999625
Jeehyae dance <jeehyae91@gmail.com>
2019-12-19 19:05:53 -0800

    added state to Login component

--- client/LogIn.js
+++ client/LogIn.js
@@ -1,20 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
-export default function App() {
+export default class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      email: ""
+    };
+  }
+
+  render() {
     return (
+      <View>
+        <Text style={styles.title}>Climbing Day</Text>
         <View style={styles.container}>
-            <Text>Rock Climbing Away~!</Text>
-            
+          <TextInput
+            style={styles.input}
+            value={this.state.userName}
+            placeholder="User Name"
+            onChangeText={userName => this.setState({ userName })}
+          />
+          <TextInput
+            style={styles.input}
+            value={this.state.email}
+            placeholder="Email"
+            onChangeText={email => this.setState({ email })}
+          />
+          <Button title="Login" />
         </View>
-    )
+        <View style={styles.needAcc}>
+          <Text>Need an account?</Text>
+          <Button title="Create Account" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 7,
+    borderColor: "black",
+    fontSize: 20,
+    margin: 10,
+    padding: 10,
+    width: 300
+  },
+  title: {
+    fontSize: 45,
+    fontWeight: "bold",
+    paddingTop: 50
+  },
+  needAcc: {
+    fontSize: 20,
+    paddingBottom: 50
+  }
+});
